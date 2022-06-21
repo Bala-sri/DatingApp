3511a87faee1c0590cd9533f18eee3af515aacb3
Bala Srinivas Tubati <balasrinivast@gmail.com>
2022-06-21 16:18:41 +0530

    commit 1

---
+++ API/Controllers/Entities/AppUser.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers.Entities
+{
+   public class AppUser
+   {
+      // public AppUser(int id, string userName)
+      // {
+      //    this.Id = id;
+      //    this.UserName = userName;
+
+      // }
+      public int Id { get; set; }
+      public string UserName { get; set; }
+   }
+}
